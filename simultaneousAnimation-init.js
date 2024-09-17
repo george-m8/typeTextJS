eb94edb6a8e9f5a0ae886bdb092a185cfd469232
georgem <geornb@gmail.com>
2024-09-17 14:57:19 +0100

    Updated demos

--- simultaneousAnimation-init.js
+++ simultaneousAnimation-init.js
@@ -2,9 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     // Hide all elements
     animatePage.hideAll(".services ul li, .contact, .email a, .social-icons a");
 
-    // Animate everything simultaneously
+    // Animate text elements sequentially
     setTimeout(() => {
-        animatePage.animateSimultaneously(".services ul li, .contact, .email a", 60, 'text');
-        animatePage.animateSimultaneously(".social-icons a", 400, 'non-text');
+
+        // Animate text elements simultaneously
+        animatePage.animateSimultaneously(".services ul li", 60, 'text');
+        // After animate the contact and email elements sequentially
+        animatePage.typeInSequence(".contact, .email a", 60, false, function() {
+            // After text animation, fade in icons sequentially
+            animatePage.fadeInSequence(".social-icons a", 400);
+        });
     }, 500); // Delay before starting the animations
 });
